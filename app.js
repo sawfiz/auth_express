5b8785eba3a40746f8e604b34fbcc93c786b4d4a
sawfiz <dashu.up@gmail.com>
2023-09-18 06:08:00 -0400

    Add index and sign-up pages

---
+++ app.js
@@ -0,0 +1,50 @@
+/////// app.js
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Database connection error:', error);
+  }
+}
+
+// Connect to the database when the app starts
+connectToDatabase();
+
+const User = mongoose.model(
+  'User',
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
